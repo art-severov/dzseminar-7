93855948d672e35e6d8e19bcad78780785594185
Artem <art-severov@yandex.ru>
2023-02-08 23:04:18 +0300

    подготовленно

---
+++ Zadacha50/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 1, 7 -> такого элемента в массиве нет
+
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matr = new int[m, n];
+Console.WriteLine("Введите номер строки: ");
+int rowuser = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int columuser = Convert.ToInt32(Console.ReadLine());
+
+FillMatrix(matr);
+FindElementMatrix(matr, rowuser, columuser);
+
+
+void FillMatrix(int[,] matrixFill)
+
+{
+    for (int i = 0; i < matrixFill.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixFill.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElementMatrix(int[,] findElem, int row, int colums)
+{
+    if (rowuser > findElem.GetLength(0) || columuser > findElem.GetLength(1))
+
+        {
+             Console.WriteLine("такого элемента нет");
+        }
+    else
+        {
+            Console.WriteLine($"значение элемента {rowuser} строки и {columuser} столбца равно {findElem[rowuser - 1, columuser - 1]}");
+        }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ zadacha47/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+
+
+
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] rowColMatrix = new double[m, n];
+Random rnd = new Random();
+
+FillMatrix(rowColMatrix);
+Console.WriteLine();
+PrintMatrix(rowColMatrix);
+
+
+void PrintMatrix(double[,] matrix)
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+
+void FillMatrix(double[,] matrix)
+
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = Convert.ToDouble(rnd.Next(-99, 100)/10.0);
+        }
+    }
+}
+
+
+
